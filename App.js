1ffe19d50fcd922b7c0b62ebe1156100562e4fc9
Enrique Barajas <enriqbar76@gmail.com>
2021-11-25 01:49:00 -0600

    ya con permisos para mapa

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useEffect} from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const buscarlocation = async () => {
+    const {status} =await Location.requestForegroundPermissionsAsync()
+    if (status !== 'granted'){
+      return Alert.alert ('No tenemos permiso para la geolacalización')
+    }
+    const location = await location.getCurrentPossitionAsync({})
+    console.log(location)
+  }
+  useEffect(() => {
+    buscarlocation() 
+  })
 
 return (
   <View style={styles.container}>
